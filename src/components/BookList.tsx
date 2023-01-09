91af0bd1a7cfaf9f93e700b3292c11f9c916fa64
Kailey <k.kotynski@gmail.com>
2023-01-09 13:35:49 -0800

    Refactored booklist, added book obj interface, list of book objs

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,28 +1,42 @@
 import { Book } from "./Book";
 
+interface BookObject {
+  title: string;
+  author: string;
+  image: string;
+}
+
+let bookObjectList: BookObject[] = [
+  {
+    title: "Reminders of Him",
+    author: "Colleen Hoover",
+    image: "https://m.media-amazon.com/images/I/617uZq23IPL._AC_UL320_.jpg",
+  },
+  {
+    title: "The Wife Before",
+    author: "Shanora Williams",
+    image: "https://m.media-amazon.com/images/I/81Ivt18vkzL._AC_UL320_.jpg",
+  },
+  {
+    title: "Twenty Years Later",
+    author: "Charlie Donlea",
+    image: "https://m.media-amazon.com/images/I/91HFUglJsjL._AC_UL320_.jpg",
+  },
+  {
+    title: "The New Couple",
+    author: "Alison James",
+    image: "https://m.media-amazon.com/images/I/61uKmK-YKwL._AC_UL320_.jpg",
+  },
+];
+
 export const BookList = () => {
   return (
     <section className="book-list">
-      <Book
-        title="Reminders of Him"
-        author="Colleen Hoover"
-        image="https://m.media-amazon.com/images/I/617uZq23IPL._AC_UL320_.jpg"
-      />
-      <Book
-        title="The Wife Before"
-        author="Shanora Williams"
-        image="https://m.media-amazon.com/images/I/81Ivt18vkzL._AC_UL320_.jpg"
-      />
-      <Book
-        title="Twenty Years Later"
-        author="Charlie Donlea"
-        image="https://m.media-amazon.com/images/I/91HFUglJsjL._AC_UL320_.jpg"
-      />
-      <Book
-        title="The New Couple"
-        author="Alison James"
-        image="https://m.media-amazon.com/images/I/61uKmK-YKwL._AC_UL320_.jpg"
-      />
+      {bookObjectList.map((book) => {
+        return (
+          <Book title={book.title} author={book.author} image={book.image} />
+        );
+      })}
     </section>
   );
 };
