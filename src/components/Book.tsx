f680b46c2c6fa1148a093c1288538f286ef4c02c
Kailey <k.kotynski@gmail.com>
2023-01-09 13:09:11 -0800

    Edited css

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -8,7 +8,7 @@ export const Book = (props: BookProps) => {
   return (
     <div className="book">
       <h1>{props.title}</h1>
-      <h2>{props.author}</h2>
+      <h3>{props.author}</h3>
       <img src={props.image}></img>
     </div>
   );
