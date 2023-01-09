ecc59ea00c2a54b842201112ae1a5cc0e1357359
Kailey <k.kotynski@gmail.com>
2023-01-09 13:56:47 -0800

    Added id to book objs, set key to id

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -4,6 +4,7 @@ interface BookObject {
   title: string;
   author: string;
   image: string;
+  id: number;
 }
 
 let bookObjectList: BookObject[] = [
@@ -11,21 +12,25 @@ let bookObjectList: BookObject[] = [
     title: "Reminders of Him",
     author: "Colleen Hoover",
     image: "https://m.media-amazon.com/images/I/617uZq23IPL._AC_UL320_.jpg",
+    id: 1,
   },
   {
     title: "The Wife Before",
     author: "Shanora Williams",
     image: "https://m.media-amazon.com/images/I/81Ivt18vkzL._AC_UL320_.jpg",
+    id: 2,
   },
   {
     title: "Twenty Years Later",
     author: "Charlie Donlea",
     image: "https://m.media-amazon.com/images/I/91HFUglJsjL._AC_UL320_.jpg",
+    id: 3,
   },
   {
     title: "The New Couple",
     author: "Alison James",
     image: "https://m.media-amazon.com/images/I/61uKmK-YKwL._AC_UL320_.jpg",
+    id: 4,
   },
 ];
 
@@ -34,7 +39,12 @@ export const BookList = () => {
     <section className="book-list">
       {bookObjectList.map((book) => {
         return (
-          <Book title={book.title} author={book.author} image={book.image} />
+          <Book
+            title={book.title}
+            author={book.author}
+            image={book.image}
+            key={book.id}
+          />
         );
       })}
     </section>
