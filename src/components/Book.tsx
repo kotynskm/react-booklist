a68464f727777dd7c19ed4dda6b858b0cd530e1c
Kailey <k.kotynski@gmail.com>
2023-01-09 16:34:37 -0800

    Added # to number, edited css styles

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -8,7 +8,7 @@ type BookProps = {
 export const Book = (props: BookProps) => {
   return (
     <div className="book">
-      <span className="number">{props.number + 1}</span>
+      <span className="number">#{props.number + 1}</span>
       <h1>{props.title}</h1>
       <h3>{props.author}</h3>
       <img src={props.image}></img>
