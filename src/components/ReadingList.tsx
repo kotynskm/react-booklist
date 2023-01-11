aade58892f1f12d5eadd57b2d665ee482906cf90
Kailey <k.kotynski@gmail.com>
2023-01-10 17:33:05 -0800

    Fixed display for book title on reading list, edited css

--- src/components/ReadingList.tsx
+++ src/components/ReadingList.tsx
@@ -13,18 +13,17 @@ export const ReadingList = () => {
   };
 
   const bookTitles = Object.keys(data);
-  bookTitles.forEach((book) => {
-    console.log(book);
-  });
+
+  console.log(data);
   return (
     <div className="reading-list">
-      <h1>My Reading List</h1>
+      <h1 className="reading-title">My Reading List</h1>
       <button onClick={handleClear}>Clear List</button>
       <ul>
         {bookTitles.map((book, index) => {
           return (
             <li>
-              {book}
+              {data[book]}
               <button value={index} onClick={handleClick}>
                 Delete
               </button>
